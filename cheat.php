d618e39722414106e39344b02eb14d48a3b0971b
PulseLane <pulselane@users.noreply.github.com>
2018-06-30 15:43:07 -0700

    Don't randomize zones

--- cheat.php
+++ cheat.php
@@ -129,12 +129,7 @@ do
 			Msg( '{green}-- Happy farming!' );
 		}
 
-		// Randomizer is here to help reduce load on Steam servers
-		// Zones are sharded, and if everyone targets the same zone, it ends up worse for everyone
-		// By giving errors like time not synced or failed to join.
-		// Everyone at level 16 or above should be able to easily reach their Rank 6 badge without a problem with bosses
-		// So please don't change this and let's get this mini game over with
-		if( $Data[ 'response' ][ 'level' ] >= 0b10000 )
+		if( $Data[ 'response' ][ 'level' ] > 25 )
 		{
 			$RandomizeZone = 1;
 
@@ -429,7 +424,7 @@ do
 			'{normal} - ETA: {green}' . $Hours . 'h ' . $Minutes . 'm (' . date_format( $Date , "jS H:i T" ) . ')'
 		);
 
-		if( $Data[ 'new_level' ] >= 0b10000 )
+		if( $Data[ 'new_level' ] > 25 )
 		{
 			$RandomizeZone = 1;
 		}
